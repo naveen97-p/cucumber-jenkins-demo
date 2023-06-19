2ece9cdda26ba7a1aa92b2db23f65a666ff9fac5
NaveenKumarPilla <pllnvnkmr9.9.97@gmail.com>
2023-06-19 11:36:53 +0530

    Updated the project with headless mode

--- src/main/java/Configurations/SetUpDriverConfigurations.java
+++ src/main/java/Configurations/SetUpDriverConfigurations.java
@@ -1,7 +1,7 @@
 package Configurations;
 
 public class SetUpDriverConfigurations {
-        public static final boolean RUN_SCRIPTS_IN_HIDE_MODE= false;
+        public static final boolean RUN_SCRIPTS_IN_HIDE_MODE= true;
         public static final boolean RUN_SCRIPTS_IN_INCOGNITO_MODE= false;
         public static final boolean ENABLE_SAVE_PASSWORDS_ALERT_EVERYTIME = false;
         public static final boolean ENABLE_CORS_CHROME_PLUGIN=false;
